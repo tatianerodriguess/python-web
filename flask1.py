d8c9a2513697030d5f5d6145dde62c987c53c2b8
Tatiane Rodrigues <that_rodrigues@hotmail.com>
2024-03-19 16:07:07 -0300

    Usando HTML
    atualização HTML

--- flask1.py
+++ flask1.py
@@ -1,15 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
  
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def index():
-    return "Página principal."
+    return render_template('indice.html')
  
 @app.route('/ola/')
 @app.route('/ola/<nome>')
 def ola(nome):
     return "Olá, " + nome
+
+@app.route('/logar' , methods=['GET', 'POST'])
+def logar():
+    if request.method == 'POST':
+        return "Recebeu post! Fazer login!"
+    else:
+        return "Recebeu get! Exibir Formulário de Login."
  
 if __name__ == '__main__':
     app.run()
